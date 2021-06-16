a28c8c73b30e41cd244a919af04b7b6ad28d8439
Juel07 <juel.braganza97@gmail.com>
2021-06-16 01:01:29 +0100

    complete challenge

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ### Intro to the Web
+
+### Screenshots
+
--- app.rb
+++ app.rb
@@ -9,14 +9,25 @@ get "/" do
   "Hello World"
 end
 
-get "/rich" do
-  erb(:index)
-end
-
 get "/secret" do
   "I get paid tomorrow.."
 end
 
 get "/paid" do
   "Here is your money: $$$$"
+end
+
+get "/random-dog" do
+  @name = ["Amigo", "Romeo", "Oreo"].sample
+  erb :index
+end
+
+get "/dog-form" do
+  erb :dog_form
+end
+
+post "/named-dog" do
+  p params
+  @name = params[:name]
+  erb :index
 end
